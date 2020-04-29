2a954df3eb4c360495fa9bb50120aa8c33505e26
Ted <ted.sandico@gmail.com>
2020-04-29 00:33:02 -0700

    added the scene and card-front and card-back

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const gameContainer = document.getElementById("game");
 const divs = document.querySelectorAll('.col')
 
+console.log("yew!")
+
+const div = document.querySelector('.card')
+div.addEventListener('click', toggle)
+
+
+// if card match cards will stay face up
+// else displayed for one second before flipping over again
+
+function addClass(e) { // this works
+  // console.dir(e.target.parentNode)
+  const parent = e.target.parentNode
+  parent.classList.add('is-flipped')
+  // remove class after x seconds per the specs
+}
+
+function toggle(e) { // this works 
+  const parent = e.target.parentNode
+  parent.classList.toggle('is-flipped');
+}
+
 const GAMETILES = 25 // represents how many game tiles there are on the screen
 const IMAGES = [
   '🤘',
